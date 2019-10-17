bc63b3761fb6f8d8830db9c515982b9e13fac00e
Bethany Holley <chocolatechipfaced@gmail.com>
2019-10-17 14:50:17 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,11 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(array)
+  while array.length > 0 do 
+    array[counter]
+    yield 
+    counter += 1 
+  end 
 end
+
+my_each(words) do |word|
+  puts word
+end 
